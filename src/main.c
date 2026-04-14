c954aaa3d3bcceb164c72b127a2907a6552916fe
kurama <paul.zenker.pz@gmail.com>
2026-04-14 15:32:37 +0200

    first version for display

--- src/main.c
+++ src/main.c
@@ -1,3 +1,155 @@
+#include "driver/i2c.h"
+#include "esp_log.h"
+
+void lcd_pulse_enable(uint8_t data);
+esp_err_t lcd_write_byte(uint8_t b);
+void lcd_pulse_enable(uint8_t data);
+void lcd_send_byte(uint8_t value, uint8_t control);
+void lcd_command(uint8_t cmd);
+void lcd_data(uint8_t data);
+void lcd_init();
+void lcd_print(const char *str);
+void lcd_set_cursor(uint8_t col, uint8_t row);
+
+static const char *TAG = "example";
+
 void app_main() {
-    
+
+    esp_rom_delay_us(1000000);
+    ESP_LOGI(TAG, "Program start");
+
+    // i2c is currently connected with PIN 13 = SDA and PIN 14 SCL
+    i2c_config_t i2c_config = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = 13,
+        .scl_io_num = 14,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = 100000,
+    };
+
+    // configure I2C
+    ESP_LOGI(TAG, "configure i2c");
+    i2c_param_config(I2C_NUM_0, &i2c_config);
+    i2c_driver_install(I2C_NUM_0, i2c_config.mode, 0, 0, 0);
+
+    ESP_LOGI(TAG, "init lcd");
+    lcd_init(); // weird sequence of commands
+
+    ESP_LOGI(TAG, "set cursor");
+    lcd_set_cursor(0, 0);
+    ESP_LOGI(TAG, "write hello");
+    lcd_print("Hello");
+
+    while (1) {
+        esp_rom_delay_us(1000000);
+        ESP_LOGI(TAG, "In while loop");
+    }
+};
+
+
+esp_err_t lcd_write_byte(uint8_t b) {
+    return i2c_master_write_to_device(
+        I2C_NUM_0, 
+        0x3F, // default address from datasheet
+        &b,
+        1,
+        1000 / portTICK_PERIOD_MS
+    );
+};
+
+/**
+ * Enable display   
+ */
+void lcd_pulse_enable(uint8_t data) {
+    lcd_write_byte(data | 0x04); // 0x04 = enable display -> set 1 at position 4
+    esp_rom_delay_us(1);
+    lcd_write_byte(data & ~0x04); // remove the 0x04 which was set before
+    esp_rom_delay_us(50);
+};
+
+/**
+ * writes data and sets display backlight on
+ * 
+ * parameters:
+ * * control: sets the set bit via or. 1 mean data; 0 means command
+ */
+void lcd_write_nibble(uint8_t nibble, uint8_t control) {
+    uint8_t data = (
+        (nibble << 4) // move data into frist 4 register bits
+        | control // set control bit at bit 0 -> control = 0 means its data
+        | 0x08 // set 4th bit to 1 to enable backlight
+    );
+    lcd_write_byte(data);
+    lcd_pulse_enable(data);
 }
+
+/**
+ * Sets a bit by setting 2 nibbles
+ * 
+ * parameters:
+ * -----------
+ * 
+ * * value:
+ * * control (bool): whether or not to set control bit (0 = data, 1 = control)
+ */
+void lcd_send_byte(uint8_t value, uint8_t control) {
+    uint8_t control_bit = control ? 0x01 : 0x00; // set control bit (first bit)
+
+    lcd_write_nibble(value >> 4, control_bit);  // first 4 characters of byte
+    lcd_write_nibble(value & 0x0F, control_bit); // last 4 characters of byte
+};
+
+void lcd_command(uint8_t cmd) {
+    lcd_send_byte(cmd, 0);
+};
+
+void lcd_data(uint8_t data) {
+    lcd_send_byte(data, 1);
+}
+
+void lcd_init() {
+    // wait ~50ms after mC turned on, to let lcd reset internally
+    vTaskDelay(50 / portTICK_PERIOD_MS);
+
+    // write 3 times 0011, which is the reset sequence. It's written 3 times, because
+    // display could be in different states
+    lcd_write_nibble(0x03, 0);  // command
+    vTaskDelay(5 / portTICK_PERIOD_MS);
+
+    lcd_write_nibble(0x03, 0);  // command
+    esp_rom_delay_us(150);
+
+    lcd_write_nibble(0x03, 0);
+
+    // switch to 4-bit mode
+    lcd_write_nibble(0x02, 0);  
+
+
+    lcd_command(0x28);  // function set (binary 0010 1000) 2 lines, 5x8 font
+    lcd_command(0x0C);  // 1100 -> set display command,turn display on, cursor off, blinking off
+    lcd_command(0x01);  // clear
+    vTaskDelay(5 / portTICK_PERIOD_MS);
+    return;
+}
+
+/**
+ * höchst räudig; sollte verboten sein; ist es auch in Rust
+ */
+void lcd_print(const char *str) {
+    while (*str) {
+        lcd_data(*str++);
+    }
+}
+
+/**
+ * parameters
+ * ----------
+ * 
+ * * col (int): which column (0-15)?
+ * * row (int): which row (0-1)?
+ */
+void lcd_set_cursor(uint8_t col, uint8_t row) {
+    uint8_t addr = (row == 0) ? 0x00 : 0x40;  // set 0100 0000 if row is 1, otherwise 0000 0000
+    lcd_command(0x80 | (addr + col)); // 0x80 (1000 0000 -> 1st / 8th bit is to set to data command)
+};
