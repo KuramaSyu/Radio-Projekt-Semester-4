9e9d6cdd960716eeec8c5bc68b24a5691c47c09f
kurama <paul.zenker.pz@gmail.com>
2026-05-15 12:55:48 +0200

    refactor: added config file for statics

---
+++ include/config.h
@@ -0,0 +1,11 @@
+/**
+ * global config file for PINs. This file was added late, hence many 
+ * statics are missing here
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define BUTTON_PIN 18
+
+#endif
